d4c401db079c3342ac4ba32f16314079ef349939
4lb10n <sektelan@gmail.com>
2024-06-13 08:03:28 +0200

    Update and rename 12.06.24.sql to Tag 3.sql

--- Tag 3.sql
+++ Tag 3.sql
@@ -2,14 +2,14 @@
 
 -- Having- Klause
 
--- HAVING ist die WHERE Klause für Gruppen
+-- HAVING ist die WHERE Klause fÃ¼r Gruppen
 -- HAVING selektiert Gruppen die duch GROUP BY definiert werden.
 
--- In der HAVING Klausel dürfen keine Aggregatfunktionen stehen
--- In der HAVING Klausel fürfen nicht nur Konstanten sondern 
--- in Unterabfragen dürfen wiederum auch Aggregatfunktionen enthalten sein
+-- In der HAVING Klausel dÃ¼rfen keine Aggregatfunktionen stehen
+-- In der HAVING Klausel fÃ¼rfen nicht nur Konstanten sondern 
+-- in Unterabfragen dÃ¼rfen wiederum auch Aggregatfunktionen enthalten sein
 
--- Gesucht ist der größte Lieferantenstatus des jeweiligen wohnortes von Lieferanten die nicht
+-- Gesucht ist der grÃ¶ÃŸte Lieferantenstatus des jeweiligen wohnortes von Lieferanten die nicht
 -- aus AAchen kommnen, wnn der durchschnittliche Statuswert am jeweiligen Ort
 -- nicht kleiner als 15 ist
 
@@ -18,14 +18,14 @@ WHERE  lstadt <> 'Aachen'
 AND AVG(status) > '15'
 GROUP BY lstadt;
 -- Diese Abgrage ist falsch
--- In der WHERE-Klausel darf kein Aggregat auftreten, es sei denn, es befindet sich in einer Unterabfrage, die in einer HAVING-Klausel oder einer Auswahlliste enthalten ist, und die Spalte, die aggregiert wird, ist ein äußerer Verweis.
+-- In der WHERE-Klausel darf kein Aggregat auftreten, es sei denn, es befindet sich in einer Unterabfrage, die in einer HAVING-Klausel oder einer Auswahlliste enthalten ist, und die Spalte, die aggregiert wird, ist ein Ã¤uÃŸerer Verweis.
 
 SELECT MAX(status), lstadt FROM lieferant
 WHERE lstadt <> 'Aachen'
 GROUP BY lstadt
 HAVING AVG(status) > '15';
 
--- Für argumente von HAVING gilt: Sie dürfen nur einen Wert pro ausgegebener
+-- FÃ¼r argumente von HAVING gilt: Sie dÃ¼rfen nur einen Wert pro ausgegebener
 -- Gruppe als Ergbnis liefern
 
 -- Nicht in jeder Abfrage macht eine Having Klausel in der GROUP BY Klausel besser
@@ -34,12 +34,12 @@ GROUP BY lnr
 HAVING anr= 'A02';
 -- nicht erlaubt
 
--- besser wäre
+-- besser wÃ¤re
 SELECT lnr, MAX(lmenge) FROM lieferung
 WHERE anr= 'A02'
 GROUP BY lnr;
 
--- ES gibt keine Beschränkung betzüglich der Anzahl von Bedingungen in den
+-- ES gibt keine BeschrÃ¤nkung betzÃ¼glich der Anzahl von Bedingungen in den
 -- Suchbedingungne der HAVING Klausel
 
 SELECT SUM(lmenge), lnr FROM lieferung
@@ -47,20 +47,20 @@ GROUP BY lnr
 HAVING MIN(lmenge) > '200' AND AVG(lmenge) < '1300';
 
 
--- generelle Übungsaufgaben
+-- generelle Ãœbungsaufgaben
 /*
 1. Schreibe eine Abfrage die folgenden Satz ausgibt:
-	In Mannheim müssten noch 400 ARtikel mit der farbe blaue lagern.
+	In Mannheim mÃ¼ssten noch 400 ARtikel mit der farbe blaue lagern.
 2. Schreibe eine Abfrage die den Namen und den Status aller Lieferanten anzeigt,
 	die im Juli 1990 geliefert haben.
-3. Schreibe eine Abfrage die den folgenden Artikel in die Tablle einfügt
+3. Schreibe eine Abfrage die den folgenden Artikel in die Tablle einfÃ¼gt
 	ein gelber Bolzen mitr einem Gewicht von 13 Gramm
 4. Gescuht sind alle Namen, Nummern und gewicht aller Artikel die am 23.07. versendet wurden. 
 5. Gesucht sind alle Wohnorte von Lieferanten, die mit H bis L beginnen.
 */
 
 -- 1:
-SELECT 'In' + astadt + ' müssen noch ', amenge,
+SELECT 'In' + astadt + ' mÃ¼ssen noch ', amenge,
 + ' Artikel mit der Farbe' + 'lagern.'
 FROM artikel
 WHERE anr = 'A03';
@@ -83,13 +83,13 @@ SELECT DISTINCT lstadt FROM lieferant
 WHERE lstadt LIKE '[HL]%';
 
 /*
-6.Gesucht ist die Menge aller grünen Artikel, minus die 200
+6.Gesucht ist die Menge aller grÃ¼nen Artikel, minus die 200
 	die wir bereits verplant aber noch nicht versendet haben.
 
-7. Gesucht sind alle Liefertan mit dem höchsten Status
+7. Gesucht sind alle Liefertan mit dem hÃ¶chsten Status
 8. Gesucht ist das durchschnittliche Gewicht aller Artikel
-9. Gesucht ist die größte Lieferung jedes Lieferanten,
-	die nach dem 23.7.90 stattgefunden hta und deren durchschnittliche Liefermenge mindestens 250 beträgt.
+9. Gesucht ist die grÃ¶ÃŸte Lieferung jedes Lieferanten,
+	die nach dem 23.7.90 stattgefunden hta und deren durchschnittliche Liefermenge mindestens 250 betrÃ¤gt.
 10. Gesucht ist der kleinste Status des jeweiligen Wohnortes von 
 	Lieferantem die nicht aus Erfurt kommen, wenn der Durchschnittliche
 	Statuswert am jeweiligen Ort nicht kleiner ist als 12
@@ -97,10 +97,10 @@ WHERE lstadt LIKE '[HL]%';
 
 -- 6:
 SELECT aname, amenge - '200' FROM artikel
-WHERE farbe= 'grün';
+WHERE farbe= 'grÃ¼n';
 
 -- 7:
-SELECT MAX(status) AS 'Höchster status' FROM lieferant;
+SELECT MAX(status) AS 'HÃ¶chster status' FROM lieferant;
 
 SELECT lname FROM lieferant
 WHERE status IN (SELECT MAX(STATUS) from lieferant);
@@ -131,17 +131,17 @@ HAVING AVG(status) >= 12;
 
 -- Unterabfragen
 
--- ist eine Abfrage innerhalb einer anderen Abfrage. Die erste Abfrage wird obere oder Äußere
+-- ist eine Abfrage innerhalb einer anderen Abfrage. Die erste Abfrage wird obere oder Ã„uÃŸere
 -- Abfrage genannt. Alle darunter liegenden werden als unter oider auch innere Abfragen bezeichnet.
 
--- Bei diesen Abfragen wird immer zuerst die innere und danach die Äußere Abfrage ausgeführt
+-- Bei diesen Abfragen wird immer zuerst die innere und danach die Ã„uÃŸere Abfrage ausgefÃ¼hrt
 
 -- Gesucht sidn die lieferanten, die in der selben Stadt leben, in Artikel A02 gelagert wird
 SELECT * FROM lieferant
 WHERE lstadt = (SELECT astadt FROM artikel
 					WHERE anr = 'A02');
 
--- gesucht sind die Lieferanten, deren Status über den durschnittlichen
+-- gesucht sind die Lieferanten, deren Status Ã¼ber den durschnittlichen
 -- Status aller Lieferanten liegt
 SELECT * FROM lieferant
 WHERE status > (SELECT AVG(status) FROM lieferant);
@@ -150,7 +150,7 @@ WHERE status > (SELECT AVG(status) FROM lieferant);
 -- es kommt vor, dass eine innere Abfrage nicht genau einen Wert als Ergebnis ausgibnt.
 -- Dieser Vergleich muss mit IN stattfinden
 
--- gesucht sind alle Angaben über Lieferanten, die bereits geliefert haben
+-- gesucht sind alle Angaben Ã¼ber Lieferanten, die bereits geliefert haben
 SELECT * FROM lieferant
 WHERE lnr IN (SELECT lnr FROM lieferung);
 
@@ -173,15 +173,15 @@ WHERE 3 <= (SELECT COUNT(*) FROM lieferung b
 
 
 -- Die Operatoren ANY und ALL
--- ALL bedeutet "größer als jeder Wert"
--- ANY bedeutet "größer als mindestens ein Wert"
+-- ALL bedeutet "grÃ¶ÃŸer als jeder Wert"
+-- ANY bedeutet "grÃ¶ÃŸer als mindestens ein Wert"
 
--- gesucht sind alle Artikel deren gewwicht größer als jeders Gewichrt der Artikel aus Ludwiegshafen
+-- gesucht sind alle Artikel deren gewwicht grÃ¶ÃŸer als jeders Gewichrt der Artikel aus Ludwiegshafen
 SELECT * FROM artikel
 WHERE gewicht > (SELECT MAX (gewicht) FROM artikel
 						WHERE astadt= 'Ludwigshafen');
 
--- Mit ALL benötigt man Befehl MAX(geicht) nicht mehr
+-- Mit ALL benÃ¶tigt man Befehl MAX(geicht) nicht mehr
 SELECT * FROM artikel
 WHERE gewicht >ALL (SELECT MAX (gewicht) FROM artikel
 						WHERE astadt= 'Ludwigshafen');
@@ -197,13 +197,13 @@ WHERE gewicht > (SELECT MIN(gewicht) FROM artikel
 						WHERE astadt = 'Ludwigshafen');
 
 
--- Generelle Übungsaufgaben
+-- Generelle Ãœbungsaufgaben
 /*
 1. Gesucht ist der Name, Nummer von allen Artikeln.
 	Zudem soll die Menge der Artikel bewertet werden. Bis 600 Artikel
 	soll nachbestellt werden, bis 1000 soll es unbedingt verkauft 
 	werden, bis 1200 sollen sie verschenkt werden. Alle Artikel mit
-	einem Bestand über 1200 sollen weggeworfen werden.
+	einem Bestand Ã¼ber 1200 sollen weggeworfen werden.
 
 2. Schreibe eine Abfrage die das aktuelle Datum ausgibt.
 
@@ -211,7 +211,7 @@ WHERE gewicht > (SELECT MIN(gewicht) FROM artikel
 	Statuswert zwischen 20 und 30
 
 4. Gesucht sind die Namen und Nummern aller Artikel, deren Gewicht
-	12, 14 oder 17 gramm beträgt.
+	12, 14 oder 17 gramm betrÃ¤gt.
 
 5. Gesucht ist der Statuswert aller Lieferanten, die am 5.8.90 und 
 	6.8.90 ausgeliefert haben.
@@ -221,7 +221,7 @@ SELECT aname, anr, amenge FROM artikel
 WHERE amenge IN (SELECT amenge FROM artikel
 						WHERE amenge= '600');
 
---lösung
+--lÃ¶sung
 SELECT anr, aname, CASE WHEN amenge BETWEEN '0' AND '600' THEN 'Nachbestellen'
 						WHEN amenge BETWEEN '601' AND '1000' THEN 'Verkaufen'
 						WHEN amenge BETWEEN '1001' AND '1200' THEN 'verwschenken'
@@ -247,11 +247,11 @@ WHERE lnr IN (SELECT lnr FROM lieferung
 
 --------------------------------------------------------------------
 -- EXISTS UND NOT EXISTS
--- Die bedingun für den Prüfsatz der äußeren Abfragen wird als Wahr ausgewertet
+-- Die bedingun fÃ¼r den PrÃ¼fsatz der Ã¤uÃŸeren Abfragen wird als Wahr ausgewertet
 -- wenn die innere Abfrage zumindest eine Ergebnisreihe liefert
 
--- Die innere Abfrage ist immer von einere Variable(Spalte) abhängig,
--- die in äußeren Abfrage berechnet wird
+-- Die innere Abfrage ist immer von einere Variable(Spalte) abhÃ¤ngig,
+-- die in Ã¤uÃŸeren Abfrage berechnet wird
 
 -- gesucht sind Ortsnamen, die Wohnort aber nicht Lagerort sind.
 SELECT lstadt FROM lieferant
@@ -263,7 +263,7 @@ SELECT * FROM lieferant
 WHERE EXISTS (SELECT * FROM lieferung
 				WHERE lieferant.lnr=lieferung.lnr);
 
--- Die Tabellen können einmalig bezeichnet werden um lieferant.lnr nicht schreiben zu müssen
+-- Die Tabellen kÃ¶nnen einmalig bezeichnet werden um lieferant.lnr nicht schreiben zu mÃ¼ssen
 SELECT * FROM lieferant a
 WHERE EXISTS (SELECT * FROM lieferung b
 				WHERE a.lnr=b.lnr);
