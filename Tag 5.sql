9995ab0f1c3c81b3a992de0b1bc4bfe661ba526c
4lb10n <sektelan@gmail.com>
2024-06-14 14:02:57 +0200

    Update and rename 14.06.24.sql to Tag 5.sql

--- Tag 5.sql
+++ Tag 5.sql
@@ -1,15 +1,15 @@
 -- 14.06.24
 
--- Einfügen von Daten
--- INSERT INTO Befehl ohne nähere definition der Daten
--- muss die Spaltenreihenfolge und der Datentyp übereinstimmt
+-- EinfÃ¼gen von Daten
+-- INSERT INTO Befehl ohne nÃ¤here definition der Daten
+-- muss die Spaltenreihenfolge und der Datentyp Ã¼bereinstimmt
 
 -- In der Reinfolge
 INSERT INTO lieferant VALUES('L07','Gaspricki', '40', 'Erfurt');
 
 SELECT * FROM lieferant;
 
--- veränderte Reihenfolge
+-- verÃ¤nderte Reihenfolge
 INSERT INTO lieferant (lstadt,lnr,lname,status)
 VALUES ('Mordor','L08','Gollum','30');
 
@@ -20,20 +20,20 @@ INSERT INTO lieferant VALUES ('L10', 'Aragon','10',NULL);
 INSERT INTO lieferant (lnr,lname) VALUES ('L09', 'Frodo');
 
 
---- Ändern von Daten
+--- Ã„ndern von Daten
 -- einfache UPDATE Anweisung
 
--- Lieferant L08 heißt jetzt Golle
+-- Lieferant L08 heiÃŸt jetzt Golle
 UPDATE lieferant SET lstadt = 'Gondor'
 WHERE lnr= 'L08';
 
--- ändern des Status der Lieferanten, die nach dme 13.08.90 geliefert haben
+-- Ã¤ndern des Status der Lieferanten, die nach dme 13.08.90 geliefert haben
 UPDATE lieferant SET status= status + 10
 FROM lieferant , lieferung
 WHERE lieferant.lnr=lieferung.lnr
 AND ldatum >= '13.08.1990';
 
--- ändern des Status in abhängigkeit mehrerer Faktoren
+-- Ã¤ndern des Status in abhÃ¤ngigkeit mehrerer Faktoren
 UPDATE lieferant 
 SET status= CASE WHEN status BETWEEN '0' AND '10' THEN status * 2.0
 				WHEN status BETWEEN '11' AND '20' THEN status * 1.3
@@ -43,29 +43,29 @@ SET status= CASE WHEN status BETWEEN '0' AND '10' THEN status * 2.0
 
 SELECT * FROM lieferant;
 
--- löschen von Daten
+-- lÃ¶schen von Daten
 
--- löschen des Fahrers Aragon
+-- lÃ¶schen des Fahrers Aragon
 DELETE FROM lieferant
 WHERE lname='Aragon';
 
--- löschen aller Lieferanten die in Gotha
+-- lÃ¶schen aller Lieferanten die in Gotha
 DELETE FROM lieferant
 WHERE lstadt= 'Gotha';
 
--- löschen aller lieferungen des lieferanten 
+-- lÃ¶schen aller lieferungen des lieferanten 
 
--- damit wir sie löschen können geben wir ihm erst eine lieferung Golle
+-- damit wir sie lÃ¶schen kÃ¶nnen geben wir ihm erst eine lieferung Golle
 INSERT INTO lieferung VALUES ('L08', 'A02','300', '19.05.1990');
 
 SELECT * FROM lieferung;
 
--- löschén seiner lieferungen
+-- lÃ¶schÃ©n seiner lieferungen
 DELETE FROM lieferung
 WHERE lnr= (SELECT lnr FROM lieferant
 			WHERE lname= 'Golle');
 
--- kleine Übungsaufgabe
+-- kleine Ãœbungsaufgabe
 -- Schreibe zwei verschiedene Abfragen die alle Lieferanten ausgibt, deren
 -- Namen mit S, C, oder B, beginnen und deren dritter Buchstabe ein a ist.
 
@@ -77,14 +77,14 @@ WHERE lname LIKE '[S,C,B]%' AND lname IN (SELECT lname FROM lieferant
 											WHERE lname LIKE '__a%');
 
 
--- große Übungsaufgabe
+-- groÃŸe Ãœbungsaufgabe
 /*
 1. Lieferannummern und Namen, die alle artikel geliefert haben
 2. Nummern, Namen und Wohnort der Lieferanten, die bereits geliefert haben
-	und deren status größer als der kleinste status aller lieferanten ist
+	und deren status grÃ¶ÃŸer als der kleinste status aller lieferanten ist
 3. Nummern und namen aller artikel, deren durchschnittliche liefermegne kleiner als die von artikel A03 ist
 4. Lieferantennamen und nummern , Artikelname und artikelnummer aller lieferungen
-	die seit dem 05.05.1990 von Hamburger lieferanten durchgeführt wurden
+	die seit dem 05.05.1990 von Hamburger lieferanten durchgefÃ¼hrt wurden
 */
 
 -- 1:
@@ -98,7 +98,7 @@ WHERE lnr IN (SELECT lname FROM lieferant)
 GROUP BY anr
 HAVING COUNT(lname);
 
--- Lösung
+-- LÃ¶sung
 
 SELECT a.lnr, a.lname FROM lieferant a JOIN lieferung b
 ON a.lnr=b.lnr
@@ -110,7 +110,7 @@ SELECT DISTINCT a.lnr, lname, lstadt FROM lieferant a JOIN lieferung b
 ON a.lnr=b.lnr
 WHERE b.lmenge >1 AND a.status > (SELECT AVG (status) FROM lieferant);
 
---Lösung
+--LÃ¶sung
 SELECT DISTINCT a.lnr, lname, lstadt FROM lieferant a JOIN lieferung b
 ON a.lnr=b.lnr
 WHERE b.lmenge >1 AND a.status > (SELECT MIN (status) FROM lieferant);
@@ -180,7 +180,7 @@ SELECT * FROM artikel;
 SELECT lnr FROM lieferung
 WHERE; 
 
---------------Lösung
+--------------LÃ¶sung
 SELECT a.lnr FROM lieferung a, lieferung b
 WHERE a.anr=b.anr
 AND b.lnr='L03'
